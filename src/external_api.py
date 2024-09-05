4e57baa1b0edcc881fc33c522154d3fe87186932
igort <igortsevelev3@gmail.com>
2024-09-05 13:06:02 +0500

    homework_13_1

--- src/external_api.py
+++ src/external_api.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import requests
 
 
-load_dotenv('/.env')
+load_dotenv("/.env")
 
 
 def get_convert(transaction: dict) -> float:
@@ -13,9 +13,9 @@ def get_convert(transaction: dict) -> float:
     if code == "RUB":
         return float(amount)
     elif code == "USD" or code == "EUR":
-        url = f'https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={code}&amount={amount}'
-        token_exchange = os.getenv('API_KEY')
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={code}&amount={amount}"
+        token_exchange = os.getenv("API_KEY")
         headers = {"apikey": token_exchange}
         response = requests.request("GET", url, headers=headers)
         result = response.json()
-        return float(result['result'])
+        return float(result["result"])
--- src/masks.py
+++ src/masks.py
@@ -1,29 +1,29 @@
 import logging
 
 
-logger = logging.getLogger('masks')
+logger = logging.getLogger("masks")
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler('../logs/masks.log', encoding='utf-8')
-file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s -%(message)s')
+file_handler = logging.FileHandler("../logs/masks.log", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s -%(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 
 
 def get_mask_card_number(card_number: int) -> str:
-    """ Функция принимает на вход номер карты и возвращает ее маску"""
+    """Функция принимает на вход номер карты и возвращает ее маску"""
     num = str(card_number)
     if len(num) != 16:
-        logger.error('Неверное количество символов')
+        logger.error("Неверное количество символов")
         raise ValueError("Неверное количество символов")
-    logger.info('Маскируем номер карты')
+    logger.info("Маскируем номер карты")
     return f'{num[:4]} {num[4:6]} {"*" * 2} {"*" * 4} {num[12:]}'
 
 
 def get_mask_account(account_number: int) -> str:
-    """ Функция принимает на вход номер счета и возвращает его маску"""
+    """Функция принимает на вход номер счета и возвращает его маску"""
     num = str(account_number)
     if len(num) != 20:
-        logger.error('Неверное количество символов')
+        logger.error("Неверное количество символов")
         raise ValueError("Неверное количество символов")
-    logger.info('Маскируем номер счета')
-    return "*" * 2 + num[- 4:]
+    logger.info("Маскируем номер счета")
+    return "*" * 2 + num[-4:]
